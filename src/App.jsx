2f062ab1fcef0ce8a972781a7e6e09252271f81e
Latif <latif_1995@live.no>
2025-09-30 11:46:10 +0200

    klargjør

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Menu></Menu>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+function Menu() {
+  return (
+    <div>
+      <button>Alle Produkter</button>
+      <button>Bruger</button>
+      <button>Pølser</button>
+    </div>
+  );
+}
+
+export default Menu;
